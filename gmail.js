23785799d09b224f18d79b32994274359ece06fd
vindhyahs <vindhu@codeunderscore.com>
2020-10-31 23:51:57 +0530

    gmail-clone

---
+++ gmail.js
@@ -0,0 +1,7 @@
+var newmail = document.getElementById('composebutton');
+newmail.addEventListener("onclick", function() {
+    var x=document.getElementById('display')
+    var y= document.createElement('div').classList.add("composebox").style. background-color="rosybrown";
+
+   
+})
